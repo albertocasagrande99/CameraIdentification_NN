46c2e2242a8129951a912f942d0f265a81fe41f8
albertocasagrande99 <alberto.casagrande@studenti.unitn.it>
2023-07-09 12:42:07 +0200

    Resume training

--- train.py
+++ train.py
@@ -17,6 +17,7 @@ def main():
     parser.add_argument("--pretrained_weights_path", required=True)
     parser.add_argument("--batch_size", required=True, type=int)
     parser.add_argument("--model_save_path", required=True)
+    parser.add_argument("--resume", required=True, type=int) #0 false - 1 true
 
     args = parser.parse_args()
 
@@ -31,7 +32,7 @@ def main():
     val_loader = DataLoader(val_dataset, batch_size=args.batch_size, num_workers=PROCESS_COUNT)
 
     model = Model(args.pretrained_weights_path)
-    camera_model = CameraModel(model=model)
+    camera_model = CameraModel(model=model, resume_training=args.resume)
 
     train(camera_model, train_loader, val_loader, val_dataset.get_labels(), args.model_save_path)
 
